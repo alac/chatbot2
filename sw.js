1327a80e0ab7159f246f0d974fb2fef2e883e279
alac <alac@users.noreply.github.com>
2026-04-13 20:43:32 -0700

    Fix empty Author's Note page

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 console.log("Service worker is active - should enable PWA functionality");
 
-const cacheName = 'chtbt2-d5b66196'; // Change value to force update
+const cacheName = 'chtbt2-4d878ad4'; // Change value to force update
 
 self.addEventListener("install", event => {
 	// Kick out the old service worker
