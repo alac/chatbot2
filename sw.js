62045597bc58866682d7168badb9e174cd0485f2
alac <alac@users.noreply.github.com>
2026-06-11 23:28:02 -0700

    Update index.html

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 console.log("Service worker is active - should enable PWA functionality");
 
-const cacheName = 'chtbt2-6201cf9f'; // Change this to force an update
+const cacheName = 'chtbt2-8f06fe13'; // Change this to force an update
 
 // Files to cache for offline use
 const filesToCache = [
