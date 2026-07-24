f8940cd754a9984f8cd636ceacf4e08c3b45d95a
alac <767203+alac@users.noreply.github.com>
2026-07-24 04:12:33 -0700

    wider number spinboxes

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 console.log("Service worker is active - should enable PWA functionality");
 
-const cacheName = 'chtbt2-1b5c10a7'; // Change this to force an update
+const cacheName = 'chtbt2-6eeb68a6'; // Change this to force an update
 
 // Files to cache for offline use
 const filesToCache = [
