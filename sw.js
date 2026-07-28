d0e35dec66b95093a6b13a75d41a354bbdfb01ac
alac <767203+alac@users.noreply.github.com>
2026-07-28 01:56:45 -0700

    DM: Force narrow dropdown on mobile

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 console.log("Service worker is active - should enable PWA functionality");
 
-const cacheName = 'chtbt2-8c707366'; // Change this to force an update
+const cacheName = 'chtbt2-bf2ba2c4'; // Change this to force an update
 
 // Files to cache for offline use
 const filesToCache = [
